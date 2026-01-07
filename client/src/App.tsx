180d79f01594d7adc56d61c737706456ea1b1996
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-01-07 19:12:25 +0500

    Complete landing page sections and pricing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 import LenisScroll from "./components/LenisScroll";
+import Generate from "./pages/Generate";
+import MyGeneration from "./pages/MyGeneration";
+import YtPreview from "./pages/YtPreview";
+import Login from "./components/Login";
 
 export default function App() {
     return (
@@ -12,6 +16,11 @@ export default function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/generate" element={<Generate />} />
+                <Route path="/generate/:id" element={<Generate />} />
+                <Route path="/my-generation" element={<MyGeneration />} />
+                <Route path="/preview" element={<YtPreview />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
             <Footer />
         </>
