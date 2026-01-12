63328d3df80a014eb11fbebee93c14f991240719
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-01-12 07:56:11 +0500

    Create backend server

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
