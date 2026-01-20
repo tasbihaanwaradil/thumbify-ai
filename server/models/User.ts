b78303c71e917acc6b1c27a46c1411227f0abf69
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-01-20 21:33:45 +0500

    Update User model schema

--- server/models/User.ts
+++ server/models/User.ts
@@ -11,7 +11,7 @@ export interface IUser extends Document {
 const UserSchema = new mongoose.Schema<IUser>({
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true, unique: true, lowercase: true },
-
+    password: { type: String, required: true },
 }, { timestamps: true })
 
 const User = mongoose.models.User || mongoose.model<IUser>('User', UserSchema)
