ac7c5fd4506685c8759e07d5234d441d037e455d
Tasbiha Anwar Adil <139496485+tasbihaanwaradil@users.noreply.github.com>
2026-03-22 13:07:12 +0500

    Connect frontend with backend, add auth flow, configs and context setup (#10)

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
 import SoftBackdrop from "./SoftBackdrop"
+import { useAuth } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
+
 
 
 const Login = () => {
     const [state, setState] = useState("login")
+    const { user, login, signUp } = useAuth()
+
+    const navigate = useNavigate()
 
     const [formData, setFormData] = useState({
         name: '',
@@ -19,8 +25,18 @@ const Login = () => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-
+        if (state === 'login') {
+            login(formData)
+        } else {
+            signUp(formData)
+        }
     }
+
+    useEffect(()=>{
+        if(user){
+            navigate('/')
+        }
+    }, [user])
 
 
     return (
